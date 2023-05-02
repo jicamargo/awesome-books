23ff1fcf46ae0a875c24e0caa66bb40f9f4b8931
jic <ji_camargo@yahoo.com>
2023-05-02 12:18:59 -0500

    add shadow to buttons, remove object unnecesary book in booklist obj

--- js/addremove.js
+++ js/addremove.js
@@ -1,53 +1,46 @@
 const booklist = {
-  books: [
-      book = {  
-        title: "",
-        author: ""
-      }
-    ],
+  books: [],
 
-  getBooks: function() {
-    this.books = JSON.parse(localStorage.getItem('bookArray')) || [];
+  getBooks() {
+    this.books = JSON.parse(localStorage.getItem('bookData')) || [];
   },
 
-  saveBooks: function(){
-    localStorage.setItem('bookArray', JSON.stringify(this.books));
+  saveBooks() {
+    localStorage.setItem('bookData', JSON.stringify(this.books));
   },
-   
-  ShowBookList: function() {
+
+  ShowBookList() {
     const bookList = document.getElementById('booklist');
-  
+
     bookList.textContent = '';
     this.books.forEach((book, index) => {
       const div = document.createElement('div');
       bookList.appendChild(div);
-      div.classList ="bookinfo " + ((index % 2 === 0) ? "grayBackground" : "whiteBackground") ;
-  
+      div.classList = `bookinfo ${(index % 2 === 0) ? 'grayBackground' : 'whiteBackground'}`;
+
       const title = document.createElement('div');
-      title.textContent = '"' + book.title + '" by ' + book.author;
+      title.textContent = `"${book.title}" by ${book.author}`;
       div.appendChild(title);
-  
- 
+
       const removeBtn = document.createElement('button');
-      removeBtn.classList ="removebtn";
+      removeBtn.classList = 'button removebtn';
       removeBtn.textContent = 'Remove';
       removeBtn.addEventListener('click', () => {
         removeBtn.parentElement.remove();
         this.books.splice(index, 1);
         this.saveBooks();
       });
-  
+
       div.appendChild(removeBtn);
-  
     });
   },
 
-  addBook: function(author, title) {
+  addBook(author, title) {
     this.books.push({ author, title });
     this.saveBooks();
     this.ShowBookList();
-  }
-}
+  },
+};
 
 const title = document.getElementById('title');
 const author = document.getElementById('author');
