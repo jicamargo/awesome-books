633fd5ec5179a01a48246a55482e009d139f2716
jic <ji_camargo@yahoo.com>
2023-05-08 16:59:20 -0500

    change addbook.js to arrow function

--- modules/addbook.js
+++ modules/addbook.js
@@ -1,7 +1,9 @@
 import saveBooks from './savebooks.js';
 
-export default function addBook(author, title, arrBooks) {
+const addBook = (author, title, arrBooks) => {
   arrBooks.push({ author, title });
   saveBooks(arrBooks);
   return arrBooks;
-}
+};
+
+export default addBook;
